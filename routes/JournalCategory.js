1dd44b1a3c3ecf78fe8a3da2674b96e98ae813e1
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2024-07-10 22:19:31 +0300

    api documentation

--- routes/JournalCategory.js
+++ routes/JournalCategory.js
@@ -6,11 +6,113 @@ module.exports = (app) => {
   const jwtCheck = jwt({ secret: process.env.APP_SECRET_TOKEN, algorithms: ["HS256"]});
   // const jwtCheck = require('../middleware/auth/authenticate');
 
+
+  /**
+ * @swagger
+ * tags:
+ *   name: JournalCategory
+ *   description: The journalCategory managing API
+ * /api/journalCategories:
+ *   get:
+ *     summary: Lists all the journalCategories
+ *     tags: [JournalCategory]
+ *     responses:
+ *       200:
+ *         description: The list of the journalCategories
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '/components/schemas/JournalCategoryComponent'
+ *   post:
+ *     summary: Create a new journalCategory
+ *     tags: [JournalCategory]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/JournalCategoryComponent'
+ *     responses:
+ *       200:
+ *         description: The created journalCategory.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/JournalCategoryComponent'
+ *       500:
+ *         description: Some server error
+ * /api/journalCategories/{id}:
+ *   get:
+ *     summary: Get the journalCategory by id
+ *     tags: [JournalCategory]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The journalCategory id
+ *     responses:
+ *       200:
+ *         description: The journalCategory response by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/JournalCategoryComponent'
+ *       404:
+ *         description: The journalCategory was not found
+ *   put:
+ *    summary: Update the journalCategory by the id
+ *    tags: [JournalCategory]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The journalCategory id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/JournalCategoryComponent'
+ *    responses:
+ *      200:
+ *        description: The journalCategory was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/JournalCategoryComponent'
+ *      404:
+ *        description: The journalCategory was not found
+ *      500:
+ *        description: Some error happened
+ *   delete:
+ *     summary: Remove the journalCategory by id
+ *     tags: [JournalCategory]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The journalCategory id
+ *
+ *     responses:
+ *       200:
+ *         description: The journalCategory was deleted
+ *       404:
+ *         description: The journalCategory was not found
+ */
+
   //  Create a new journalCategory
   router.post('/', jwtCheck, PostValidator, journalCategory.createRecord);
 
   // Retrieve all journalCategory data
-  router.get('/', jwtCheck, journalCategory.fetchRecords);
+  router.get('/', journalCategory.fetchRecords);
 
   // Retrieve journalCategory data
   router.get('/:id', jwtCheck, journalCategory.fetchRecord);
